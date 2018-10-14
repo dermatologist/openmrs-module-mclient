7c7fba148dfab237b7c3ec242ea1939c783ecd61
dermatologist <github@gulfdoctor.net>
2018-10-13 22:12:35 -0400

    pom upgraded with dependency versions

---
+++ api/src/main/java/org/openmrs/module/mclient/api/impl/KafkaProducerServiceImpl.java
@@ -0,0 +1,6 @@
+package org.openmrs.module.mclient.api.impl;
+
+import org.openmrs.module.atomfeed.api.writers.impl.DefaultFeedWriter;
+
+public class KafkaProducerServiceImpl extends DefaultFeedWriter {
+}
