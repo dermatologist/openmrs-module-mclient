ebf5918b90b08709d9d1d5826f5667ccd2ea9af1
dermatologist <github@gulfdoctor.net>
2018-10-15 18:42:48 -0400

    saveEvent override

--- api/src/main/java/org/openmrs/module/mclient/MessagePublisherClient.java
+++ api/src/main/java/org/openmrs/module/mclient/MessagePublisherClient.java
@@ -9,6 +9,7 @@ import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +18,7 @@ import static org.openmrs.module.mclient.MessagingClientConfig.KAFKA_HOST;
 import static org.openmrs.module.mclient.MessagingClientConfig.KAFKA_PORT;
 import static org.openmrs.module.mclient.MessagingClientConfig.KAFKA_TOPIC;
 
+@Component
 public class MessagePublisherClient {
     private static final Logger LOG = LoggerFactory.getLogger(MessagePublisherClient.class);
 
